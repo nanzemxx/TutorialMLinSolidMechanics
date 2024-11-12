06d66a3284787736502c97bc011a119ed78f1d98
xin <xiongxinstu@icloud.com>
2024-11-12 11:46:50 +0100

    Task2.3

--- Task2_Group Xin and Huasen/train/models.py
+++ Task2_Group Xin and Huasen/train/models.py
@@ -28,16 +28,17 @@ _x_to_y: custom trainable layer
 
 """
 
-class _F_to_P(layers.Layer):
+class _C_to_P(layers.Layer):
     def __init__(self):
-        super(_F_to_P, self).__init__()
+        super(_C_to_P, self).__init__()
         self.ls = [layers.Dense(4, 'softplus')]
+        self.ls += [layers.Dense(4, 'softplus')]
         self.ls += [layers.Dense(9, activation='linear')]
     
-    def call(self, F):
+    def call(self, C):
         for l in self.ls:
-            F = l(F)
-        return F
+            C = l(C)
+        return C
 # %%   
 """
 main: construction of the NN model
@@ -46,11 +47,11 @@ main: construction of the NN model
 
 def main(**kwargs):
     # define input shape
-    Fs = tf.keras.Input(shape=[9])
+    Cs = tf.keras.Input(shape=[6])
     # define which (custom) layers the model uses
-    Ps = _F_to_P(**kwargs)(Fs)
+    Ps = _C_to_P(**kwargs)(Cs)
     # connect input and output
-    model = tf.keras.Model(inputs = [Fs], outputs = [Ps])
+    model = tf.keras.Model(inputs = [Cs], outputs = [Ps])
     # define optimizer and loss function
     model.compile('adam', 'mse')
     return model
